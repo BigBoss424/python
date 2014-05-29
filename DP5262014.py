f3197363a0938ff34191cb92a849af6b96100b6d
BigBoss424 <ajones061392@gmail.com>
2014-05-29 10:42:41 -0700

    This is the programming exercise from r/dailyprogrammer of the date 5-26-2014. So far I've been able to only get Hello World to print out. Have much more to learn about python to get the rest of the list done. Note: List of things to implement are in the comment section above.

---
+++ DP5262014.py
@@ -0,0 +1,15 @@
+# The Following are tasks that need to be implemented
+# 1) Output Hello World to the console
+# 2) Return an array of the first 100 numbers that are
+# divisible by 3 and 5.
+# 3) Create a program that verifies if a word is an
+# an anagram of another word
+# 4) Create a program that removes a specified letter
+# from a word.
+# 5) Sum all the elements of an array
+
+# Hello World in Python
+
+print "Hello World!"
+
+#
